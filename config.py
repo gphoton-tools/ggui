b58e5d06468382123f36babbafd465ac3ddd16df
Duy Nguyen <dtn5ah@virginia.edu>
2017-06-14 17:58:16 -0400

    Changed Data_Factory to Importer

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 from glue.core import Data
-from glue.config import data_factory
 from glue.core.data_factories import has_extension
+from glue.app.qt.application import GlueApplication
+
+from glue.config import data_factory, importer, menubar_plugin
 
 from gPhoton.gphoton_utils import read_lc as read_lc
 
-@data_factory('gPhoton Lightcurve', has_extension('csv'), default='csv')
-def gPhoton_data(filename):
+from PyQt5.QtWidgets import QFileDialog
+
+#@data_factory('gPhoton Lightcurve', has_extension('csv'), default='csv')
+#def gPhoton_data(filename):
+
+@importer("gPhoton Lightcurve Importer")
+def gPhoton_data():
+
+#@menubar_plugin("Load gPhoton CSV Plot")
+#def my_plugin(session, data_collection):
     """
     Data loader customized for 'typical' gPhoton CSV Lightcurves, 
         specifically laoding t_mean and flux_bgsub
@@ -13,9 +23,23 @@ def gPhoton_data(filename):
     This function extracts calibrated fluxes (flux_bgsub) and mean 
         timestamps (t_mean) and makes plot
     """
+
+    # Set File Dialog Options
+    caption = ("Select gPhoton CSV Lightcurve file")
+    dialog = QFileDialog(caption=caption)
+    dialog.setFileMode(QFileDialog.ExistingFile)
+    dialog.setNameFilter("Lightcurve CSV (*.csv)")
+
+    # Prompt User for file to import
+    dialog.exec_()
+
+    # Get File Name (Need to grab first file name)
+    filename = dialog.selectedFiles()[0]
+    
     dataArray = []
 
     # Read in CSV file using gPhoton.gphoton_utils.read_lc
+    print(filename)
     csvRaw = read_lc(filename)
 
     # Create Data Object from glue using imported two columns from CSV
@@ -23,4 +47,8 @@ def gPhoton_data(filename):
 
     # Return Array of Data
     dataArray.append(csvData)
+
     return dataArray
+
+
+
