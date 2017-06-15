2afe3f032c3566ab8a117e2b26f65b03d3ef0f81
Duy Nguyen <dtn5ah@virginia.edu>
2017-06-15 14:08:03 -0400

    Skeleton for v0.2

--- config.py
+++ config.py
@@ -1,21 +1,16 @@
-from glue.core import Data
-from glue.core.data_factories import has_extension
+from glue.core import Data, DataCollection
+from glue.core.data_factories import has_extension, load_data
 #from glue.app.qt.application import GlueApplication
+from glue.viewers.scatter.qt import ScatterWidget
 
-from glue.config import data_factory, importer, menubar_plugin
+from glue.config import menubar_plugin
 
 from gPhoton.gphoton_utils import read_lc as read_lc
 
 from PyQt5.QtWidgets import QFileDialog
 
-#@data_factory('gPhoton Lightcurve', has_extension('csv'), default='csv')
-#def gPhoton_data(filename):
-
-@importer("gPhoton Lightcurve Importer")
-def gPhoton_data():
-
-#@menubar_plugin("Load gPhoton CSV Plot")
-#def my_plugin(session, data_collection):
+@menubar_plugin("Load gPhoton CSV Plot")
+def my_plugin(session, data_collection):
     """
     Data loader customized for 'typical' gPhoton CSV Lightcurves, 
         specifically laoding t_mean and flux_bgsub
@@ -24,6 +19,29 @@ def gPhoton_data():
         timestamps (t_mean) and makes plot
     """
 
+    # Prompt User via File Dialog for LightCurve CSVs
+
+    # Prompt User via File Dialog for CoAdd Fits
+
+    # Prompt User via File Dialog for Image Cube Fits
+
+
+    # Import Lightcurve CSVs to DataCollection()
+
+    # Import CoAdd Fits to DataCollection()
+
+    # Import Image Cube Fits to DataCollection()
+
+
+    # Generate 2D ScatterPlot Canvas for Lightcurve CSVs
+
+    # Generate 2D Image Viewer Canvas for CoAdd Fits
+
+    # Generate 2D Image Viewer Canvas for Image Cube Fits
+
+    return
+
+    """
     # Set File Dialog Options
     caption = ("Select gPhoton CSV Lightcurve file")
     dialog = QFileDialog(caption=caption)
@@ -47,8 +65,13 @@ def gPhoton_data():
 
     # Return Array of Data
     dataArray.append(csvData)
+    data_collection.append(csvData)
+    #dc.append(load_data('cr_dra_lc.csv'))
+    sw = ScatterWidget(session)
+    sw.add_data(csvData)
+    sw.register_to_hub()
 
-    return dataArray
+    #return dataArray
+    return
 
-
-
+    """
