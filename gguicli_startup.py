5d063d610069af33247fe99541a2ae50bb8b207f
Duy Nguyen <dtn5ah@virginia.edu>
2018-12-07 22:21:04 -0500

    #aqx4g Code restructuring: Removed deprecated preFixedLayoutTab methods, moved autochop methods to autochop module

--- gguicli_startup.py
+++ gguicli_startup.py
@@ -15,154 +15,7 @@ import yaml
 import datetime
 
 import qtTabLayouts
-
-X_MONITOR_RES = 1080
-WIN_OS_X_OFFSET = 145
-x_glue_win_size = X_MONITOR_RES - WIN_OS_X_OFFSET
-
-Y_MONITOR_RES = 1920
-WIN_OS_Y_OFFSET = 320
-y_glue_win_size = Y_MONITOR_RES - WIN_OS_Y_OFFSET
-
-def create_scatter_canvas(dataToDisplay, x_att, y_att, glueApp, window_title=None, plot_title=None, x_min=None, x_max=None, x_window_size=None, y_window_size=None, x_window_pos=None, y_window_pos=None):
-    """
-    Function to generate new scatter widget. (Designed for modularity and organization)
-
-    :param dataToDisplay: Glue (Pandas) Data object to display on scatter widget
-    :type dataToDisplay: glue.core.data.Data
-
-    :param xatt: Index of Glue Data Object to display along x axis
-    :type xatt: str
-
-    :param yatt: Index of Glue Data Object to display along y axis
-    :type yatt: str
-
-    :param plot_title: Title to be displayed on graph
-    :type plot_tite: str
- 
-    :param xmin: Minimum value of X Axis
-    :type xmin: numpy.float64
-
-    :param xmax: Maximum value of X Axis
-    :type xmax: numpy.float64
-
-    :param glueApp: Current instantiation of the Glue Application to spawn canvas into
-    :type glueApp: glue.app.qt.application.GlueApplication
-    """
-    # Note for devs: Import inside function due to Glue Startup Script Workorder
-    #from glue.viewers.scatter.qt import ScatterViewer
-    #sv = ScatterViewer(glueApp.session)
-    #sv.add_data(dataToDisplay)
-
-    sv = qtTabLayouts.duyScatterViewer(glueApp.session)
-    sv.add_data(dataToDisplay)
-    return sv
-    # Generate new scatter widget
-    scatterCanvas = glueApp.new_data_viewer(ScatterViewer, dataToDisplay)
-    # Set Scatter Canvas Attributes
-    if window_title: scatterCanvas.setWindowTitle(window_title)
-    if plot_title: scatterCanvas.axes.set_title(plot_title)
-    scatterCanvas.state.x_att = dataToDisplay.id[x_att]
-    scatterCanvas.state.y_att = dataToDisplay.id[y_att]
-    if x_min: scatterCanvas.state.x_min = x_min
-    if x_max: scatterCanvas.state.x_max = x_max
-    if x_window_size and y_window_size: scatterCanvas.viewer_size = x_window_size, y_window_size
-    if x_window_pos and y_window_pos: scatterCanvas.position = x_window_pos, y_window_pos
-
-    return scatterCanvas
-
-def create_image_canvas(imageDataToDisplay, glueApp, window_title=None, plot_title=None):
-    """
-    Function to generate a new image widget. (Designed for consistency with above)
-
-    :param imageDataToDisplay: Glue (Pandas) Data object containing image intensity array
-    :type imageDataToDisplay: glue.core.data.Data
-
-    :param glueApp: Current instantiation of the Glue Application to spawn canvas into
-    :type glueApp: glue.app.qt.application.GlueApplication
-    """
-    # Note for devs: Import inside function due to Glue Startup Script Workorder
-    #from glue.viewers.image.qt import ImageViewer
-    sv = qtTabLayouts.duyImageViewer(glueApp.session)
-    sv.add_data(imageDataToDisplay)
-    return sv
-    
-    # Generate new Image Widget
-    imageCanvas = glueApp.new_data_viewer(ImageViewer, imageDataToDisplay)
-    if window_title: imageCanvas.setWindowTitle(window_title)
-    if plot_title: imageCanvas.axes.set_title(plot_title)
-    return imageCanvas
-
-def lightcurveChopList(parentData, axis, timeInterval):
-    """
-    Breaks observation data into observations separated by timeInterval. Returns list of times
-    
-    :param parentData: Glue (Pandas) Data Object containing CSV lightcurve data
-    :type parentData: glue.core.data.Data
-
-    :param axis: parameter to split across (usual = time)
-    :type axis: string
-
-    :param timeInterval: interval/amount to split parameter 'axis' across
-    :type timeInterval: numpy.float64
-
-    :returns: list -- List of autochop regions
-    """
-    import numpy
-    # Calculate all time differences between points
-    timeDifferences = numpy.diff(parentData[axis])
-    # Find all indices of blank jumps greater than timeInterval
-    obsWindows = []
-    # Determine first observation window
-    obsStart = parentData[axis, 0]
-    for index, difference in enumerate(timeDifferences):
-       # If the time difference is larger than specified time 
-       if difference > timeInterval:
-            # End time is the current index
-            obsEnd = parentData['MeanTime', index]
-            # Append this time window as tuple to master array
-            obsWindows.append((index, obsStart, obsEnd))
-            # Next window begins in next data point
-            obsStart = parentData['MeanTime', index + 1]
-    # Add last window to master array
-    obsWindows.append((len(parentData[axis]), obsStart, parentData['MeanTime', -1]))
-    return obsWindows
-
-def lightcurveChopImport(glueApp, dataCollection, parentData, obsWindows):
-    """
-    Receives list of obs windows, breaks dataseries accordingly, imports data object to collection
-    
-    :param glueApp: Current instantiation of the Glue Application to spawn canvas into
-    :type glueApp: glue.app.qt.application.GlueApplication
-
-    :param dataCollection: Library of imported data objects to current Glue interface
-    :type dataCollection: glue.core.data.DataCollection 
-
-    :param parentData: Glue (Pandas) Data Object containing CSV lightcurve data
-    :type parentData: glue.core.data.Data
-
-    :param obsWindows: List of observation windows with indices upon which to chop
-    :type obsWindows: list
-    """
-    from glue.core import Data
-    # Set Initial Window start to index 0
-    indxStart = 0
-    # Get List of all extensions
-    extensionList = parentData.component_ids()
-    for window in obsWindows:
-        # Grab ending index from list
-        indxEnd = window[0]
-        # Instantiate new Data Object container for the chop
-        newChop = Data(label="AutoChop " + str(indxStart))
-        for extension in extensionList:
-            # Break every extension into segments defined by indxStart and indxEnd
-            newChop[str(extension)] = parentData[extension][indxStart:indxEnd]
-        # Import completed chop into dataCollection
-        dataCollection.append(newChop)
-        # Display Chop (for debugging purposes)
-        #create_scatter_canvas(newChop,'MeanTime','Flux_BackgroundSubtracted',glueApp)
-        # Move new index start to next index beyond current window
-        indxStart = indxEnd + 1
+from autochop import lightcurveChopList, lightcurveChopImport
 
 def getGguiDataProducts():
     """
@@ -236,88 +89,6 @@ def getGguiDataProducts():
         print("Unrecognized character")
         exit(-1)
 
-def loadTarget(glueApp, fixedTab, dataCollection, targetName, targetFiles):
-    """
-    Given a specific 
-
-    :returns: dict -- Dictionary of gGui Targets and corresponding file locations
-    """
-    def loadLightcurve(glueApp, fixedTab, dataCollection, targ_name, lcFileName):
-        print(lcFileName)
-        # Load Data from file. Add to current Data Collection
-        csvData = load_data(lcFileName)
-        dataCollection.append(csvData)
-        # Cleanse raw csvData to only t_mean and flux_bgsub.
-        # Add that data "subset" (not glue subset, just a subset) to Data Collection.
-        # Must be in Data Collection to use
-        lightcurveData = Data(Flux_BackgroundSubtracted=csvData['flux_bgsub'],
-                            MeanTime=csvData['t_mean'],
-                            label='Lightcurve of ' + lcFileName)
-        dataCollection.append(lightcurveData)
-        # AutoChop Lightcurve and import those chops
-        obsWindows = lightcurveChopList(lightcurveData, "MeanTime", 3600)
-        lightcurveChopImport(glueApp, dataCollection, lightcurveData, obsWindows)
-        # Generate 2D ScatterPlot Canvas for Lightcurve CSVs and add to Glue
-        lcWidget = create_scatter_canvas(lightcurveData, 
-                            'MeanTime',
-                            'Flux_BackgroundSubtracted', 
-                            glueApp,
-                            window_title=("Full Lightcurve of: " + lcFileName),
-                            plot_title='Lightcurve of ' + targ_name,
-                            x_window_size =   y_glue_win_size,
-                            y_window_size =   floor(x_glue_win_size/3),
-                            x_window_pos  =   1,
-                            y_window_pos  =   1
-        )
-        fixedTab.loadLightcurve(lcWidget)
-        #fixedTab.addSubWindow(lcWidget)
-        #fixedTab.setActiveSubWindow(lcWidget)
-
-    def loadCoadd(glueApp, fixedTab, dataCollection, targ_name, coaddFileName):
-        print(coaddFileName)
-        # Load Image from file
-        fitsImage = load_data(coaddFileName)
-        # Import Image to Data Collection for plotting
-        dataCollection.append(fitsImage)
-        # Generate 2D Image Viewer Canvas for coadd Images
-        caWidget = create_image_canvas(fitsImage, 
-                            glueApp, 
-                            window_title=('CoAdd Image of: ' + coaddFileName),
-                            plot_title='CoAdd of ' + targ_name,
-                            x_window_size   =   floor(y_glue_win_size/2),
-                            y_window_size   =   floor(x_glue_win_size*(2.0/3.0)),
-                            x_window_pos    =   1,
-                            y_window_pos    =   floor(x_glue_win_size/3)
-        )
-        fixedTab.loadCoadd(caWidget)
-        #fixedTab.addSubWindow(caWidget)
-
-    def loadCube(glueApp, fixedTab, dataCollection, targ_name, cubeFileName):
-        print(cubeFileName)
-        # Load Image from file
-        fitsImage = load_data(cubeFileName)
-        # Import Image to Data Collection for plotting
-        dataCollection.append(fitsImage)
-        # Generate 2D Image Viewer Canvas for Image Cube Fits
-        cubeWidget = create_image_canvas(fitsImage,
-                            glueApp,
-                            window_title=('3D Image Cube of: ' + cubeFileName),
-                            plot_title=('Cube of ' + targ_name + ': [' + str(datetime.datetime.now()) + ']'),
-                            x_window_size   =   floor(y_glue_win_size/2),
-                            y_window_size   =   floor(x_glue_win_size*(2.0/3.0)),
-                            x_window_pos    =   floor(y_glue_win_size/2),
-                            y_window_pos    =   floor(x_glue_win_size/3)
-        )
-        fixedTab.loadCube(cubeWidget)
-        #fixedTab.addSubWindow(cubeWidget)
-
-    # Defines which function loads which datatype
-    loadFunctions = {'lightcurve': loadLightcurve, 'coadd': loadCoadd, 'cube': loadCube}
-    # For each target file, call its corresponding load function
-    for dataProductType in targetFiles:
-        #dataProductType, filePath = gGuiDataProductListing
-        loadFunctions[dataProductType](glueApp, fixedTab, dataCollection, targetName, targetFiles[dataProductType])
-
 def main():
     # Initialize Glue Application with blank Data Collection
     dataCollection = DataCollection()
@@ -347,6 +118,10 @@ def main():
     #fixedTab.tileSubWindows()
     #print("###: " + str(fixedTab.activeSubWindow()))
     
+    # Note for later. This is how you autochop :P
+    #obsWindows = lightcurveChopList(lightcurveData, "MeanTime", 3600)
+    #lightcurveChopImport(glueApp, dataCollection, lightcurveData, obsWindows)
+
     #start Glue
     glueApp.start()
 
