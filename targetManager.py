51a0a36da90cc1d364265a3da8526e04e1657c18
Duy Nguyen <dtn5ah@virginia.edu>
2018-12-13 23:00:04 -0500

    Initial prototype for Target Manager

---
+++ targetManager.py
@@ -0,0 +1,20 @@
+from qtpy import QtWidgets
+from glue.config import menubar_plugin
+
+class targetManager(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.targetCatalog = {}
+
+    def loadTargets(self, targDict):
+        self.targetCatalog.update(targDict)]
+    
+    def getTarget(self, targName):
+        return self.targetCatalog.get(targName, {})
+
+
+@menubar_plugin("Do something")
+def my_plugin(session, data_collection):
+    
+    return
