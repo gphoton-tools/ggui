4897f09bce840f68f85959c823bd73387e930e90
Duy Nguyen <dtn5ah@virginia.edu>
2018-12-17 23:24:21 -0500

    Target Manager handles gGui YAML loading; started primary target scaffholding

--- targetManager.py
+++ targetManager.py
@@ -1,14 +1,33 @@
 from qtpy import QtWidgets
 from glue.config import menubar_plugin
 
+import yaml
+from glue.core.data_factories import load_data
+
 class targetManager(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.targetCatalog = {}
+        self.primaryTarget = None
 
-    def loadTargets(self, targDict):
-        self.targetCatalog.update(targDict)]
+    def loadGguiYaml(self, gguiYamlPath):
+        self.targetCatalog.update(yaml.load(open(gguiYamlPath, 'r')))
+
+    def loadTargetDict(self, targDict):
+        self.targetCatalog.update(targDict)
+
+    def setPrimaryTarget(self, targName):
+        self.primaryTarget = targName
+    
+    def getPrimaryData(self):
+        targetFiles = self.targetCatalog.get(self.primaryTarget)
+        dataDict = {}
+        for dataProductType in targetFiles:
+            dataDict[dataProductType] = load_data(targetFiles[dataProductType])
+        return dataDict
+    
+
     
     def getTarget(self, targName):
         return self.targetCatalog.get(targName, {})
