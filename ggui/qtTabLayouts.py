ac72d3d3dcf8528c1f2dd837e36906ccc42f3eca
Duy Nguyen <dtn5ah@virginia.edu>
2019-06-07 22:45:46 -0400

    Overview Tab fallbacks to skip x_att, y_att assignment if no config value provided

--- ggui/qtTabLayouts.py
+++ ggui/qtTabLayouts.py
@@ -185,7 +185,7 @@ class ggui_overview_tab(QtWidgets.QMdiArea):
         # For all the data we've been given, call the appropriate constructor with that data
         for dataType, data in target_data.items():
             try:
-                viewer_setters[dataType](session, target_name, data, config.get('Mandatory Fields', dataType + "_x"), config.get('Mandatory Fields', dataType + "_y"))
+                viewer_setters[dataType](session, target_name, data, config.get('Mandatory Fields', dataType + "_x", fallback=None), config.get('Mandatory Fields', dataType + "_y", fallback=None))
             except ValueError as error:
                 print("WARNING: " + str(error))
                 continue
