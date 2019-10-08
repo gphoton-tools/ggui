3a0aaca4ed5e12e86f6e3eb8ec389b98f46779fc
Duy Nguyen <dtn5ah@virginia.edu>
2019-10-08 17:34:10 -0400

    setup.py uses new version file

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except FileNotFoundError:
 about = {}
 if not VERSION:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    with open(os.path.join(here, project_slug, "version.py")) as f:
         exec(f.read(), about)
 else:
     about['__version__'] = VERSION
